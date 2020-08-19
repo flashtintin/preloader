f15139833f45e03fe517192077b6f0d9943a34fd
kid <dingyi@cmiot.chinamobile.com>
2020-08-19 15:26:24 +0800

    update

--- test/dist/index.js
+++ test/dist/index.js
@@ -1,1 +1,113 @@
-!function(e){var o={};function t(n){if(o[n])return o[n].exports;var r=o[n]={i:n,l:!1,exports:{}};return e[n].call(r.exports,r,r.exports,t),r.l=!0,r.exports}t.m=e,t.c=o,t.d=function(e,o,n){t.o(e,o)||Object.defineProperty(e,o,{enumerable:!0,get:n})},t.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},t.t=function(e,o){if(1&o&&(e=t(e)),8&o)return e;if(4&o&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(t.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&o&&"string"!=typeof e)for(var r in e)t.d(n,r,function(o){return e[o]}.bind(null,r));return n},t.n=function(e){var o=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(o,"a",o),o},t.o=function(e,o){return Object.prototype.hasOwnProperty.call(e,o)},t.p="",t(t.s=0)}([function(e,o,t){"use strict";t.r(o);new class{constructor(e,o={mode:"no-cors",method:"GET"}){"string"==typeof e?(console.log("string is good"),this.initSingle(e,o)):e instanceof Array?console.log("array is good"):console.log("bad type")}initSingle(e,o){const t=Object.assign(o,{responseType:"blob"});fetch(e,t).then(e=>{console.log(e)}).then(e=>{console.log("done",e)}).catch(e=>{console.log(e)})}}("http://pic1.win4000.com/wallpaper/f/5876e4b550212.jpg")}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./test/test.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/preloader.js":
+/*!**************************!*\
+  !*** ./src/preloader.js ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return Preloader; });\nclass Preloader {\n  constructor(resource, option = { mode: \"no-cors\", method: \"GET\" }) {\n    if (typeof resource === \"string\") {\n      console.log(\"string is good\");\n      this.initSingle(resource, option);\n    } else if (resource instanceof Array) {\n      console.log(\"array is good\");\n    } else {\n      console.log(\"bad type\");\n    }\n\n    // this.test = new Test();\n  }\n\n  initSingle(r, o) {\n    const option = Object.assign(o, { responseType: \"blob\" });\n    fetch(r, option)\n      .then((response) => {\n        // if (response.ok) {\n        //   return response.blob();\n        // }\n        // throw new Error(\"Network response was not ok.\");\n        console.log(response);\n      })\n      .then((blob) => {\n        console.log(\"done\", blob);\n      })\n      .catch((e) => {\n        console.log(e);\n      });\n  }\n}\n\n\n//# sourceURL=webpack:///./src/preloader.js?");
+
+/***/ }),
+
+/***/ "./test/test.js":
+/*!**********************!*\
+  !*** ./test/test.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _src_preloader_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../src/preloader.js */ \"./src/preloader.js\");\n\n\nconst preloader = new _src_preloader_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"](\n  \"http://pic1.win4000.com/wallpaper/f/5876e4b550212.jpg\"\n);\n\n\n//# sourceURL=webpack:///./test/test.js?");
+
+/***/ })
+
+/******/ });
--- webpack.test.config.js
+++ webpack.test.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./test/test.js",
+  mode: "development",
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "test/dist"),
